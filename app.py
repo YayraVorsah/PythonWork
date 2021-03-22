6c992caf1f87263d1a0aa88323e84c11ba267906
YayraVorsah <yavprincess@gmail.com>
2021-03-22 16:32:57 +0000

    Initial python project

---
+++ app.py
@@ -0,0 +1,25 @@
+print("Yayra Vorsah")
+print('o----')
+print('  |||| ')
+print('*' * 10)
+
+
+price = 10   #number
+print(price)
+
+rating = 4.9     #float
+name = "yayra"      #string
+is_published = False        #boolean
+
+
+
+
+name_of_person = "Ama Velar"
+age_of_person = 40
+is_new_here = True
+
+
+name = input("What is your name?")
+colour = input("What is your favourite colour?")
+print('Hi' +" "+ name)
+print(name + ", likes " + colour)
