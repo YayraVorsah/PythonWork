19af70b7cbf15e4c5844be848def3e633e259373
YayraVorsah <yavprincess@gmail.com>
2021-03-22 22:05:27 +0000

    conversionn

---
+++ conversion.py
@@ -0,0 +1,20 @@
+# weight_lbs = input("weight (lbs):")
+# weight_kg = (int(weight_lbs)) * 0.45
+# print(weight_kg)
+
+
+# course = 'Python for "Biginners"'
+#   #       012345 
+# #print(course[0:3])
+# print(course[-9:-2])
+
+# name = :"Jennifer"
+# print(name[1:-1])  #ennife
+
+
+first = "yayra"
+last = "Ama"
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder'
+print(msg)
+print(message)
