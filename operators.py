e691a7f9c1f66f6433c2f5897d8aa3b55c35fd37
YayraVorsah <yavprincess@gmail.com>
2021-03-23 13:15:55 +0000

    operators

---
+++ operators.py
@@ -0,0 +1,30 @@
+# x = 10
+# x = x + 5           # can be rewritten as x += 5
+# print(x)
+
+#OPERATOR PRECEDENCE
+
+x = 10 + 3 * 2      #exponent
+print(x)               #multiplication / division
+                            #addition or subtraction
+
+# NOTE THAT THE BRACKETS TAKE PRECEDENCE
+# X = (2 + 3) * 10 -3    ANS WOULD BE 47
+# 
+# 
+x = 2.9
+print(round(x))       # round the number to the nearest whole number
+
+print("Absolute ")
+
+x = -2.9
+print(abs(x)) # always prints the positive number 
+
+
+
+print("MATH FUNCTIONS")
+import math
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
+print(math.exp(2))
