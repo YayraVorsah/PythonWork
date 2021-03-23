f2b0c170611f5136937be9f10d45eef20c3c8ca4
YayraVorsah <yavprincess@gmail.com>
2021-03-23 12:06:37 +0000

    string methods

---
+++ stringMethods.py
@@ -0,0 +1,8 @@
+course = 'Python for Beginners'
+#print(len(course))
+#print(course.lower())  # .upper(), .find('Beginners'), .title
+#print(course.replace('P',"J"))
+
+#in operator 
+course = 'Python for me'
+print('me' in course) # would produce a boolean operator(True/False) be mindful of your case
